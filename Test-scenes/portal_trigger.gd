8f7fe4a2f93004520fe798c5b2e0fa99cc2ee6e5
fredgeorgebxl <freddgeorge@yahoo.com>
2018-12-13 22:01:45 +0100

    test camera angle

--- Test-scenes/portal_trigger.gd
+++ Test-scenes/portal_trigger.gd
@@ -10,6 +10,6 @@ func _on_Portal_body_entered(body):
 	var wall_visible = get_parent().visible
 	if wall_visible && body.has_method("is_player"):
 		get_node("/root/global").change_level(scene, tag)
-		#var pivot = get_node("../../camera-system/pivot")
-		#var product = pivot.get_global_transform().basis.z.angle_to(get_global_transform().basis.z)
-		#print(rad2deg(product))
+		var pivot = get_node("../../camera-system/pivot")
+		var product = pivot.get_global_transform().basis.x.angle_to(get_global_transform().basis.z)
+		print(rad2deg(product))
