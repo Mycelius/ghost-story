7f1698b9bc696c3e2c6bff5d3855d0a2066893f0
Fred-George <f.george@visit.brussels>
2019-03-19 17:41:31 +0100

    infinite level next step

--- Test-scenes/tests_scenes/infinite_level.gd
+++ Test-scenes/tests_scenes/infinite_level.gd
@@ -4,15 +4,15 @@ onready var tile = load("res://infinite_tile.tscn")
 
 var TILE_SIZE = 4.0
 var tiles = []
-var current_tile = Vector2(0.0, 0.0)
+var current_tile_pos = Vector2(0.0, 0.0)
 
 func _ready():
 	init_tiles()
 
 func _player_entered(entered):
-	if entered != current_tile:
+	if entered != current_tile_pos:
 		update_tiles(entered)
-		current_tile = entered
+		current_tile_pos = entered
 	
 func init_tiles():
 	for i in range(3):
@@ -21,11 +21,29 @@ func init_tiles():
 			add_tile(i, j)
 
 func update_tiles(entered):
-	pass
-	
+	if current_tile_pos.x - entered.x > 0:
+		shift_tiles(1,"x")
+	elif current_tile_pos.x - entered.x < 0:
+		shift_tiles(-1, "x")
+		
+	if current_tile_pos.y - entered.y > 0:
+		shift_tiles(1,"y")
+	elif current_tile_pos.y - entered.y < 0:
+		shift_tiles(-1, "y")
+
+func shift_tiles(direction, axis):
+	var new_tiles = []
+	for i in range(3):
+		new_tiles.append([])
+		if axis == "y":
+			pass
+		for j in range(3):
+			if axis == "x":
+				pass
+
 func add_tile(i, j):
-	var x = TILE_SIZE * (i - 1.0)
-	var y = TILE_SIZE * (j - 1.0)
+	var x = current_tile_pos.x + TILE_SIZE * (i - 1.0)
+	var y = current_tile_pos.y + TILE_SIZE * (j - 1.0)
 	tiles[i].append(tile.instance())
 	tiles[i][j].translate(Vector3(x,0.0,y))
 	tiles[i][j].connect("player_entered", self, "_player_entered", [Vector2(x, y)])
